86f261e951097054c7b10ce39f575087b5bd0a2c
cgpvicto-2444639 <2444639@etudiant.cegepvicto.ca>
2026-08-26 18:47:27 -0400

    Ajout de la balle et de la camera qui fonctionne

--- Assets/Scripts/CibleCamera.cs
+++ Assets/Scripts/CibleCamera.cs
@@ -11,6 +11,21 @@ public class CibleCamera : MonoBehaviour
     [SerializeField, Tooltip("Vitesse de déplacement en m/s")]
     private float vitesseDeplacement;
 
+    [SerializeField, Tooltip("Vitesse de rotation en deg/sec")]
+    private float vitesseRotation;
+
+    [SerializeField, Tooltip("Vitesse inclinaison en deg/sec")]
+    private float vitesseInclinaison;
+
+    [SerializeField, Tooltip("Angles d'inclinaison limites de la caméra. Doit être plus petit que le premier angle ou plus grand que le second")]
+    private Vector2 limitesInclinaison;
+
+    [SerializeField, Tooltip("Vitesse zoom de la caméra")]
+    private float vitesseZoom;
+
+    [SerializeField, Tooltip("Limites du zoom de la caméra")]
+    private Vector2 limitesZoom;
+
     [Header("Références aux objets de jeu")]
     [SerializeField, Tooltip("Le PlayerInput qui gère les actions de la personne qui joue")]
     private PlayerInput controles;
@@ -27,6 +42,20 @@ public class CibleCamera : MonoBehaviour
     /// </summary>
     private Vector2 deplacement;
 
+    /// <summary>
+    /// La rotation actuelle de la caméra
+    /// </summary>
+    private float rotation;
+
+    /// <summary>
+    /// L'inclinaison actuelle de la caméra
+    /// </summary>
+    private float inclinaison;
+
+    /// <summary>
+    /// Le zoom actuel de la caméra
+    /// </summary>
+    private float zoom;
 
     private void Start()
     {
@@ -34,11 +63,26 @@ public class CibleCamera : MonoBehaviour
         InputAction actionDeplacement = controles.actions.FindAction("player/DeplacerCamera");
         actionDeplacement.performed += CommencerDeplacement;
         actionDeplacement.canceled += TerminerDeplacement;
+
+        InputAction actionRotation = controles.actions.FindAction("player/TournerCamera");
+        actionRotation.performed += CommencerRotation;
+        actionRotation.canceled += TerminerRotation;
+
+        InputAction actionInclinaison = controles.actions.FindAction("player/InclinerCamera");
+        actionInclinaison.performed += CommencerInclinaison;
+        actionInclinaison.canceled += TerminerInclinaison;
+
+        InputAction actionZoom = controles.actions.FindAction("player/ZoomerCamera");
+        actionZoom.performed += CommencerZoom;
+        actionZoom.canceled += TerminerZoom;
     }
 
     private void Update()
     {
         DeplacerCamera();
+        TournerCamera();
+        InclinerCamera();
+        ZoomerCamera();
     }
 
     private void OnDestroy()
@@ -49,6 +93,18 @@ public class CibleCamera : MonoBehaviour
         InputAction actionDeplacement = controles.actions.FindAction("player/DeplacerCamera");
         actionDeplacement.performed -= CommencerDeplacement;
         actionDeplacement.canceled -= TerminerDeplacement;
+
+        InputAction actionRotation = controles.actions.FindAction("player/TournerCamera");
+        actionRotation.performed -= CommencerRotation;
+        actionRotation.canceled -= TerminerRotation;
+
+        InputAction actionInclinaison = controles.actions.FindAction("player/InclinerCamera");
+        actionInclinaison.performed -= CommencerInclinaison;
+        actionInclinaison.canceled -= TerminerInclinaison;
+
+        InputAction actionZoom = controles.actions.FindAction("player/ZoomerCamera");
+        actionZoom.performed -= CommencerZoom;
+        actionZoom.canceled -= TerminerZoom;
     }
 
     #region Déplacement
@@ -77,12 +133,10 @@ public class CibleCamera : MonoBehaviour
     {
         if (deplacement.sqrMagnitude > 0.0f)
         {
-            Vector3 prochainePosition = transform.position +
-                transform.right * deplacement.x * Time.deltaTime +
+            Vector3 prochainePosition = transform.position -
+                transform.right * deplacement.x * Time.deltaTime -
                 transform.forward * deplacement.y * Time.deltaTime;
-
             prochainePosition = Vector3.Scale(prochainePosition, new Vector3(1.0f, 0.0f, 1.0f));
-
 
             if (volumeCamera.bounds.Contains(prochainePosition))
             {
@@ -90,5 +144,102 @@ public class CibleCamera : MonoBehaviour
             }
         }
     }
-}
     #endregion
+
+    #region Rotation
+    /// <summary>
+    /// Commence la rotation de la caméra
+    /// </summary>
+    /// <param name="contexte">Information du callback de l'action</param>
+    private void CommencerRotation(InputAction.CallbackContext contexte)
+    {
+        rotation = vitesseRotation * contexte.ReadValue<float>();
+    }
+
+    /// <summary>
+    /// Termine la rotation de la caméra
+    /// </summary>
+    /// <param name="contexte">Information du callback de l'action</param>
+    private void TerminerRotation(InputAction.CallbackContext contexte)
+    {
+        rotation = 0.0f;
+    }
+
+    /// <summary>
+    /// Effectue la rotation de la caméra
+    /// </summary>
+    private void TournerCamera()
+    {
+        // Important de tourner dans l'espace du monde pour ne pas avoir d'interaction avec l'inclinaison de la caméra
+        transform.Rotate(new Vector3(0.0f, rotation * Time.deltaTime, 0.0f), Space.World);
+    }
+    #endregion
+
+    #region Inclinaison 
+    /// <summary>
+    /// Commence l'inclinaison de la caméra
+    /// </summary>
+    /// <param name="contexte">Information du callback de l'action</param>
+    private void CommencerInclinaison(InputAction.CallbackContext contexte)
+    {
+        inclinaison = vitesseInclinaison * contexte.ReadValue<float>();
+    }
+
+    /// <summary>
+    /// Termine l'inclinaison de la caméra
+    /// </summary>
+    /// <param name="contexte">Information du callback de l'action</param>
+    private void TerminerInclinaison(InputAction.CallbackContext contexte)
+    {
+        inclinaison = 0.0f;
+    }
+
+    /// <summary>
+    /// Applique l'inclinaison de la caméra en fonction de l'input du joueur et des limites d'inclinaison
+    /// </summary>
+    private void InclinerCamera()
+    {
+        float angle = (transform.localEulerAngles.x + inclinaison * Time.deltaTime) % 360;
+
+        if (angle < limitesInclinaison.x || angle > limitesInclinaison.y)
+        {
+            transform.Rotate(new Vector3(inclinaison * Time.deltaTime, 0.0f, 0.0f), Space.Self);
+        }
+    }
+    #endregion
+
+    #region Zoom
+    /// <summary>
+    /// Commence le zoom de la caméra
+    /// </summary>
+    /// <param name="contexte">Information du callback de l'action</param>
+    private void CommencerZoom(InputAction.CallbackContext contexte)
+    {
+        zoom = vitesseZoom * contexte.ReadValue<float>();
+    }
+
+    /// <summary>
+    /// Termine le zoom de la caméra    
+    /// </summary>
+    /// <param name="contexte">Information du callback de l'action</param>
+    private void TerminerZoom(InputAction.CallbackContext contexte)
+    {
+        zoom = 0.0f;
+    }
+
+    /// <summary>
+    /// Applique le zoom de la caméra en fonction de l'input du joueur
+    /// </summary>
+    private void ZoomerCamera()
+    {
+        CinemachinePositionComposer positionComposer = cameraGeree.GetComponent<CinemachinePositionComposer>();
+        Vector3 offsetCamera = positionComposer.TargetOffset + positionComposer.TargetOffset.normalized * zoom;
+        float distanceCamera = offsetCamera.magnitude;
+
+        if (distanceCamera >= limitesZoom.x && distanceCamera <= limitesZoom.y)
+        {
+            positionComposer.TargetOffset = offsetCamera;
+        }
+    }
+    #endregion
+}
